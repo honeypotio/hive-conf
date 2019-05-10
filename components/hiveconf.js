124bda1be44ace5c1fb4d355ecf4f43275d2cdb1
Mirko Nitschke <mn@honeypot.io>
2019-05-10 17:19:46 +0200

    HiveConf 18 Section until first slider

---
+++ components/hiveconf.js
@@ -0,0 +1,248 @@
+import PageBreaks from '../utils/page-breaks';
+import Carousel from 'react-bootstrap/Carousel';
+
+const Testimonials = [{
+  "title" : "Share Experiences Across Teams",
+  "name" : "Teddy",
+  "position" : "Talent Manager",
+  "text" : "Finally a conference where I can take my CTO to and make use of the learnings to make a game plan for our recruitment. I really loved to see people who were interested in how to improve recruitment in their companies! After all, we are all in it and the sooner we become \"partners in crime\" (Thanks Kevin!) the better!"
+},
+{
+  "title" : "Share Experiences Across Teams",
+  "name" : "Teddy Tod",
+  "position" : "Talent Manager",
+  "text" : "I really loved to see people who were interested in how to improve recruitment in their companies! After all, we are all in it and the sooner we become \"partners in crime\" (Thanks Kevin!) the better!"
+}]
+
+export default function Hiveconf() {
+  return (
+    <div id={"hiveconf18"}>
+      <div className={"wrapper"}>
+        <div className={"oval"}></div>
+        <h2>HiveConf'18</h2>
+        <div className={"description"}>
+          <p>Last year, HiveConf brought together 200 attendees, from C-level executives to team leads and managers in HR and Tech. With 22 speakers and topics including scaling tech teams, optimizing organizational design and preparing engineering pipelines, the first edition of HiveConf was a blast!  Let’s check out some highlights from last year’s event!
+          </p>
+          <img src={`/static/images/nav-bg.jpg`} alt={"HiveConf 18 Video"} />
+        </div>
+        <div className={"feedback"}>
+          <div className={"thumbsup"}>
+            <img src={`/static/icons/ThumbsUp.svg`} alt={"Thumps Up"}/>
+          </div>
+          <p><strong>97% of our attendees</strong> shared that they are likely to come back for HiveConf’19</p>
+        </div>
+      </div>
+      <div>
+      <div className={"carouselcontainer"}>
+        <div className={"carouseloval"}></div>
+          <Carousel 
+          indicators={false}
+          nextIcon={<img aria-hidden="true" src={`/static/icons/ArrowYellow_Right.svg`} />}
+          prevIcon={<img aria-hidden="true" src={`/static/icons/ArrowYellow_Left.svg`} />}>
+            {Testimonials.map(testimonial => (
+              <Carousel.Item>
+                <div className={"test"}>
+                  <img className={"quotesign"} src={`/static/icons/Quote.svg`} alt={"Quote Signs"}/>
+                  <div className={"testimonial"}>
+                    <div className={"left"}>
+                      <h3 className={"title"}>{testimonial.title}</h3>
+                      <div className={"info"}>
+                        <h3 className={"name"}>{testimonial.name}</h3>
+                        <div className={"position"}>{testimonial.position}</div>
+                      </div>
+                    </div>
+                    <div className={"right"}>
+                      <p>{testimonial.text}</p>
+                    </div>
+                  </div>     
+                </div>       
+              </Carousel.Item>
+            ))}
+          </Carousel>
+      </div>
+      </div>
+    
+    <style jsx>{`
+    #hiveconf18 {
+      background: linear-gradient(90deg, #0000FF 0%, #0000AA 100%);
+    }
+    
+    .wrapper {
+      max-width: 1100px;
+      margin: 0 auto;
+      padding: 20px 0;
+      position: relative;
+      z-index: 1;
+    }
+    @media  ${ PageBreaks.smUp } {
+      .wrapper {
+        padding: 150px 0 60px 0;
+      }
+    }
+
+    h2 {
+      font-size: 36px;
+      color: white;
+    }
+    @media ${ PageBreaks.smUp } {
+      h2 {
+        font-size: 60px;
+        margin-top: 0;
+        margin-bottom: 40px;
+      }
+    }
+
+    .oval {	
+      height: 413px;
+      width: 413px;
+      border-radius: 50%;	
+      background: linear-gradient(90deg, #0000FF 0%, #0000AA 100%);
+      position: absolute;
+      left: 100px;
+      z-index: -1;
+    }
+    @media  ${ PageBreaks.smUp } {
+      .oval {
+        top: -40px;
+      }
+    }
+
+    .description {
+      display: grid;
+        width: 80%;
+        margin: 0 auto;
+        grid-template-columns: repeat(2, calc(50% - 15px));
+        grid-column-gap: 30px;
+        grid-template-rows: auto;
+    }
+
+    .description p {
+      color: white;
+      font-size: 18px;
+      line-height: 46px;
+    }
+
+    .description img {
+      max-width: 100%;
+      max-height: 90%;
+      align-self: center;
+    }
+
+    .feedback {
+      width: 60%;
+      margin: 100px auto 0 auto;
+      display: grid;
+      grid-template-columns: auto auto;
+      grid-column-gap: 32px;
+    }
+
+    .thumbsup {
+      background: linear-gradient(90deg, #0000FF 0%, #0000AA 100%);
+      width: 105px;
+      height: 105px;
+      border-radius: 50%;
+      position: relative
+    }
+    .thumbsup img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      margin: auto;
+    }
+
+    .feedback p {
+      font-size: 27px;
+      line-height: 52px;
+      color: white;
+      margin: 0;
+    }
+
+    .carouselcontainer {
+      position: relative;
+      height: 650px;
+    }
+
+    .carouselcontainer > :global(.carousel) {
+      position: absolute;
+      top: 50%;
+      left: 0;
+      right: 0;
+      transform: translateY(-50%);
+    }
+    .carouselcontainer > :global(.carousel) > :global(.carousel-inner) {
+      max-width: 1100px;
+      margin: 0 auto;
+      height: 500px;
+    }
+
+    .carouseloval {
+      width: 650px;
+      height: 650px;
+      background: linear-gradient(90deg, #0000FF 0%, #0000AA 100%);
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      border-radius: 50%;
+    }
+    
+    .placer {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+
+    .testimonial {
+      display: grid;
+      grid-template-columns: repeat(2, calc(50% - 15px));
+      grid-column-gap: 30px;
+      grid-template-rows: auto;
+      color: white;
+    }
+
+    .testimonial p {
+      font-size: 18px;
+      line-height: 46px;
+      margin: 0;
+    }
+    
+    .testimonial h3 {
+      font-weight: bold;
+      font-size: 36px;
+      line-height: 44px;
+    }
+
+    .testimonial .left {
+      position: relative;
+    }
+
+    .testimonial .info {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+    }
+    
+    .testimonial .position {
+      font-size: 24px;
+      font-weight: 500;
+      line-height: 45px;
+
+    }
+
+    .quotesign {
+      width: 80px;
+      margin-bottom: 50px;
+    }
+
+    .test {
+      width: 80%; 
+      margin: 0 auto;
+    }
+    `}</style>
+    </div>
+  )
+}
