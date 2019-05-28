2bba273fb51f9e2f3d4f9dce6c4a26df0a4e2e44
Moira McAulay <moira.mcaulay.info@gmail.com>
2019-05-28 10:19:56 +0200

    fix layout of About Honeypot

--- components/sponsorships.js
+++ components/sponsorships.js
@@ -2,8 +2,8 @@ import PageBreaks from '../utils/page-breaks';
 
 export default function Sponsorships() {
   return (
-    <div id={"sponsorship"} className={"wrapper"}>
-      <div >
+    <div id={"sponsorship"} >
+      <div className={"wrapper"}>
         <div className={"oval"}></div>
         <h2>Partner with us</h2>
         <p>HiveConf is an excellent opportunity to reach CTOs, CHROs, VPs of Engineering, and Talent Acquisition Leaders in the industry. Contact our partnership team at <a target="_blank" href="mailto:hiveconf@honeypot.io" className={"active-link"}>hiveconf@honeypot.io</a> to learn about sponsorship opportunities and ways to highlight your brand.</p>
